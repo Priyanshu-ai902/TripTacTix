932dbd82c4a84e21ea12392737a324a7e5225f6f
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-09-02 19:47:06 +0530

    react-dom added with routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <h1 className='font-bold text-2xl '>hello world</h1>
+     <Button>psjcoder</Button>
     </>
   )
 }
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div>
+      create-trip
+    </div>
+  )
+}
+
+export default CreateTrip
